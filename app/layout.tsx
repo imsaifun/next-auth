6a3e4164c8d6397e0e7d9ddb8252c4c5436a7eb5
imsaifun <imsaifun@gmail.com>
2023-05-17 14:02:23 +0400

    working

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import { Inter } from 'next/font/google'
+import AuthContext from './context/AuthContext'
+import ToasterContext from './context/ToasterContext'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+    title: 'Home - Messanger',
+    description: 'A Messanger clone built with Next.js and TypeScript.',
+}
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <AuthContext>
+                    <ToasterContext />
+                    {children}
+                </AuthContext>
+            </body>
+        </html>
+    )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}'
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    container: {
-      center: true,
-      padding: {
-        DEFAULT: '1rem',
-        md: '1.5rem',
-        lg: '2rem'
-      }
+    extend: {
+      
     },
-    extend: {}
   },
-  plugins: []
+  plugins: [
+    require('@tailwindcss/forms')({
+      strategy: 'class',
+    }),
+  ],
 }
